8389c31e58c13273d4c3badd94cf287fc249ff63
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2024-11-27 12:19:37 -0300

    Create OMZ-plugins-setup.sh

---
+++ OMZ-plugins-setup.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+# Script para instalar plugins do Oh My Zsh
+echo "[*] Instalando plugins do Oh My Zsh..."
+
+# Lista de plugins a serem instalados
+plugins=("zsh-autosuggestions" "zsh-syntax-highlighting" "zsh-completions")
+
+for plugin in "${plugins[@]}"; do
+    # Verifica se o plugin já está instalado
+    if [ ! -d "$ZSH/custom/plugins/$plugin" ]; then
+        echo "[*] Instalando plugin: $plugin..."
+        git clone "https://github.com/zsh-users/$plugin.git" "$ZSH/custom/plugins/$plugin"
+    else
+        echo "[*] Plugin $plugin já está instalado."
+    fi
+done
+
+# Atualizando o arquivo .zshrc para ativar os plugins
+echo "[*] Configurando .zshrc..."
+if ! grep -q "plugins=(" "$HOME/.zshrc"; then
+    echo "plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions)" >> "$HOME/.zshrc"
+else
+    sed -i 's/plugins=(/plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-completions /' "$HOME/.zshrc"
+fi
+
+# Recarregar a configuração do Zsh
+echo "[*] Recarregando o Zsh..."
+source "$HOME/.zshrc"
+
+echo "[*] Plugins instalados e configurados com sucesso!"
