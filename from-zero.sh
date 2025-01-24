734fa5291bb0c58d07e795b8a89014480e927847
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2025-01-24 15:23:30 -0300

    Update from-zero.sh

--- from-zero.sh
+++ from-zero.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-pkg update; pkg upgrade -y; pkg install git; git clone https://github.com/museu-do-novo/termux-scripts.git; cd termux-scripts; chmod +x *.sh; bash my-termux-setup.sh
+pkg update; pkg upgrade -y; pkg install -y git; git clone https://github.com/museu-do-novo/termux-scripts.git; cd termux-scripts; chmod +x *.sh; bash my-termux-setup.sh
