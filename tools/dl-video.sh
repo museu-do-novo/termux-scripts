4bb43eaa480bcec20ad267bbecbfb5e6ccc63f7c
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2025-02-04 20:16:56 -0300

    Create dl-video.sh

---
+++ tools/dl-video.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dl-video() {
+    local link="${1:-$(termux-clipboard-get)}"
+    local output_path="${2:-$HOME/storage/movies/%(title)s.%(ext)s}"
+
+    yt-dlp -o "$output_path" --write-auto-sub --sub-lang en "$link" && \
+    termux-toast "video baixado" || termux-toast "erro"
+}
+
+dl-video "$1" "$2"
