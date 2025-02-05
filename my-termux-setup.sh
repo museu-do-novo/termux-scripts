24c3f5e4f81763c49b155a845eca4192d8d38881
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2025-02-04 21:22:34 -0300

    Update my-termux-setup.sh

--- my-termux-setup.sh
+++ my-termux-setup.sh
@@ -3,25 +3,35 @@
 cd
 clear
 echo "[*] Iniciando configuração do Termux..."
-
-# Configurando permissões de armazenamento
-echo "[*] Configurando permissões de armazenamento..."
-termux-setup-storage
-clear
+sleep 2
 
 # Mudando espelho de repositório
 echo "[*] Alterando espelho de repositórios..."
 sleep 2
 termux-change-repo
 clear
-------------------------------------------------------------
-# Lista de pacotes a serem instalados
-packages=("tmux" "x11-repo" "tesseract" "root-repo" "tur-repo" "python3" "git" "wget" "nmap" "netcat-openbsd" "termux-x11-nightly" "termux-api" "android-tools" "openssh" "htop" "zsh" "nodejs" "curl" "aria2" "axel" "ffmpeg" "libqrencode" "zbar")
 
 # Atualizando repositórios e pacotes
 echo "[*] Atualizando pacotes..."
+sleep 2
 pkg update && pkg upgrade -y
 clear
+
+# Configurando permissões de armazenamento
+echo "[*] Configurando permissões de armazenamento..."
+sleep 2
+termux-setup-storage
+clear
+
+------------------------------------------------------------
+echo "[*] Instalando pacotes..."
+sleep 2
+# Lista de pacotes a serem instalados
+packages=("tmux" "x11-repo" "tesseract" "root-repo" \
+"tur-repo" "python3" "git" "wget" "nmap" \
+"netcat-openbsd" "termux-x11-nightly" "termux-api" \
+"android-tools" "openssh" "htop" "zsh" "nodejs" \
+"curl" "aria2" "axel" "ffmpeg" "libqrencode" "zbar")
 
 # Instalando pacotes
 for package in "${packages[@]}"; do
@@ -34,11 +44,10 @@ for package in "${packages[@]}"; do
     clear
 done
 ------------------------------------------------------------
-
-python_packages=("yt-dlp" "instaloader")
-
-# Atualizando repositórios e pacotes
+# Intalando pacotes do python
 echo "[*] PYTHON: Instalando pacotes..."
+sleep 2
+python_packages=("yt-dlp" "instaloader")
 
 # Instalando pacotes
 for package in "${python_packages[@]}"; do
@@ -53,27 +62,18 @@ for package in "${python_packages[@]}"; do
 
 ------------------------------------------------------------
 
-# URLs e arquivos APK para download e instalação
-termux_api_url="https://f-droid.org/repo/com.termux.api_51.apk"
-hackers_keyboard_url="https://f-droid.org/repo/org.pocketworkstation.pckeyboard_1041001.apk"
-vlc_url="https://f-droid.org/repo/org.videolan.vlc_13050736.apk"
-
-
-termux_api_apk=~/storage/downloads/termux-api.apk 
-hackers_keyboard_apk=~/storage/downloads/hackers-keyboard.apk
-vlc_apk=~/storage/dowloads/vlc.apk
-
-
-download_urls=("$termux_api_url" "$hackers_keyboard_url" "$vlc_url")
-apk_files=("$termux_api_apk" "$hackers_keyboard_apk" "$vlc_apk")
-# Baixando e instalando APKs
-for i in "${!download_urls[@]}"; do
-    echo "[*] Baixando: ${download_urls[i]}..."
-    wget -O "${apk_files[i]}" "${download_urls[i]}"
-    if [[ $? -ne 0 ]]; then
-        echo "[!] Erro ao baixar o APK: ${download_urls[i]}"
-    fi
-done
+# URLs dos APK para download
+download_path="~/storage/downloads/"
+download_urls=(\
+"https://f-droid.org/repo/com.termux.api_51.apk" \
+"https://f-droid.org/repo/org.pocketworkstation.pckeyboard_1041001.apk" \
+"https://f-droid.org/repo/org.videolan.vlc_13050736.apk" \
+)
+wget -O "$download_path" "${download_urls[@]}"
+if [[ $? -ne 0 ]]; then
+    echo "[!] Erro de download!"
+    exit 1
+fi
 
 ------------------------------------------------------------
 
@@ -86,9 +86,9 @@ else
 fi
 
 # script em zsh para adicionar os plugins
-if [ -f "$PWD/termux-scripts/OMZ-plugins-setup.sh" ]; then
+if [ -f ./OMZ-plugins-setup.sh ]; then
     echo "[*] Executando configuração de plugins para Oh My Zsh..."
-    zsh "$PWD/termux-scripts/OMZ-plugins-setup.sh"
+    zsh ./OMZ-plugins-setup.sh
 else
     echo "[!] Script de configuração de plugins não encontrado: OMZ-plugins-setup.sh"
 fi
