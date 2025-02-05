fcbeb5496859968d03b8a7d2348351857b101d5b
museu-do-novo <138774203+museu-do-novo@users.noreply.github.com>
2025-02-04 21:28:59 -0300

    Update my-termux-setup.sh

--- my-termux-setup.sh
+++ my-termux-setup.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 # Script para configurar o Termux com ferramentas essenciais e Oh My Zsh
+
+# Função para mensagens coloridas
+echo_color() {
+    local color_code=""
+    case $1 in
+        red) color_code="\e[31m";;       # Erros
+        green) color_code="\e[32m";;     # Sucesso
+        yellow) color_code="\e[33m";;    # Avisos
+        blue) color_code="\e[34m";;      # Informação
+        magenta) color_code="\e[35m";;   # Destaque 1
+        cyan) color_code="\e[36m";;      # Destaque 2
+        white) color_code="\e[37m";;     # Texto normal
+        *) color_code="\e[0m";;          # Padrão (sem cor)
+    esac
+    echo -e "${color_code}$2\e[0m"
+}
+
 cd
 clear
-echo "[*] Iniciando configuração do Termux..."
+echo_color "cyan" "[*] Iniciando configuração do Termux..."
 sleep 2
 
 # Mudando espelho de repositório
-echo "[*] Alterando espelho de repositórios..."
+echo_color "cyan" "[*] Alterando espelho de repositórios..."
 sleep 2
 termux-change-repo
 clear
 
 # Atualizando repositórios e pacotes
-echo "[*] Atualizando pacotes..."
+echo_color "cyan" "[*] Atualizando pacotes..."
 sleep 2
-pkg update  && pkg upgrade -y
+pkg update && pkg upgrade -y
 clear
 
 # Configurando permissões de armazenamento
-echo "[*] Configurando permissões de armazenamento..."
+echo_color "cyan" "[*] Configurando permissões de armazenamento..."
 sleep 2
 termux-setup-storage
 clear
 
-------------------------------------------------------------
-echo "[*] Instalando pacotes..."
+# ------------------------------------------------------------
+echo_color "magenta" "------------------------------------------------------------"
+echo_color "cyan" "[*] Instalando pacotes..."
 sleep 2
+
 # Lista de pacotes a serem instalados
 packages=("tmux" "x11-repo" "tesseract" "root-repo" \
 "tur-repo" "python3" "git" "wget" "nmap" \
@@ -35,64 +54,66 @@ packages=("tmux" "x11-repo" "tesseract" "root-repo" \
 
 # Instalando pacotes
 for package in "${packages[@]}"; do
-    echo "[*] Instalando: $package..."
+    echo_color "cyan" "[*] Instalando: $package..."
     pkg install -y "$package"
     if [[ $? -ne 0 ]]; then
-        echo "[!] Erro ao instalar $package. Abortando..."
+        echo_color "red" "[!] Erro ao instalar $package. Abortando..."
         exit 1
     fi
     clear
 done
-------------------------------------------------------------
-# Intalando pacotes do python
-echo "[*] PYTHON: Instalando pacotes..."
+
+# ------------------------------------------------------------
+# Instalando pacotes do python
+echo_color "cyan" "[*] PYTHON: Instalando pacotes..."
 sleep 2
 python_packages=("yt-dlp" "instaloader")
 
-# Instalando pacotes
 for package in "${python_packages[@]}"; do
-    echo "[*] Instalando: $package..."
+    echo_color "cyan" "[*] Instalando: $package..."
     pip install "$package"
     if [[ $? -ne 0 ]]; then
-        echo "[!] Erro ao instalar $package. Abortando..."
+        echo_color "red" "[!] Erro ao instalar $package. Abortando..."
         exit 1
     fi
     clear
+done
 
-
-------------------------------------------------------------
-
+# ------------------------------------------------------------
 # URLs dos APK para download
+echo_color "magenta" "------------------------------------------------------------"
 download_path="~/storage/downloads/"
 download_urls=(\
 "https://f-droid.org/repo/com.termux.api_51.apk" \
 "https://f-droid.org/repo/org.pocketworkstation.pckeyboard_1041001.apk" \
 "https://f-droid.org/repo/org.videolan.vlc_13050736.apk" \
 )
+
+echo_color "cyan" "[*] Baixando APKs..."
 wget -O "$download_path" "${download_urls[@]}"
 if [[ $? -ne 0 ]]; then
-    echo "[!] Erro de download!"
+    echo_color "red" "[!] Erro de download!"
     exit 1
 fi
 
-------------------------------------------------------------
-
+# ------------------------------------------------------------
 # Instalando Oh My Zsh
+echo_color "magenta" "------------------------------------------------------------"
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    echo "[*] Instalando Oh My Zsh..."
+    echo_color "cyan" "[*] Instalando Oh My Zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
-    echo "[*] Oh My Zsh já está instalado."
+    echo_color "cyan" "[*] Oh My Zsh já está instalado."
 fi
 
-# script em zsh para adicionar os plugins
+# Script em zsh para adicionar os plugins
 if [ -f ./OMZ-plugins-setup.sh ]; then
-    echo "[*] Executando configuração de plugins para Oh My Zsh..."
+    echo_color "cyan" "[*] Executando configuração de plugins para Oh My Zsh..."
     zsh ./OMZ-plugins-setup.sh
 else
-    echo "[!] Script de configuração de plugins não encontrado: OMZ-plugins-setup.sh"
+    echo_color "red" "[!] Script de configuração de plugins não encontrado: OMZ-plugins-setup.sh"
 fi
 
 # Mensagem final
-echo "[*] Configuração geral concluída!"
+echo_color "green" "[*] Configuração geral concluída!"
 exit 0
