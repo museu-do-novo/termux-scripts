6753924c8f8f8ca732af614d84e35b72351d8a40
museu-do-novo <luzdanieldasilva@outlook.com>
2025-02-15 13:21:05 -0300

    melhorias

--- tools/dl-audio.sh
+++ tools/dl-audio.sh
@@ -4,7 +4,7 @@ dl-audio() {
     local link="${1:-$(termux-clipboard-get)}"
     local output_path="${2:-$HOME/storage/music/%(title)s.%(ext)s}"
 
-    yt-dlp -o "$output_path" -x --audio-format mp3 "$link" && \
+    yt-dlp --cookies ./yt.netscape -o "$output_path" -x --audio-format mp3 "$link" && \
     termux-toast "audio baixado" || termux-toast "erro"
 }
 
--- tools/dl-video.sh
+++ tools/dl-video.sh
@@ -4,7 +4,7 @@ dl-video() {
     local link="${1:-$(termux-clipboard-get)}"
     local output_path="${2:-$HOME/storage/movies/%(title)s.%(ext)s}"
 
-    yt-dlp -o "$output_path" --write-auto-sub --sub-lang en "$link" && \
+    yt-dlp --cookies ./yt.netscape -o "$output_path" --write-auto-sub --sub-lang en "$link" && \
     termux-toast "video baixado" || termux-toast "erro"
 }
 
